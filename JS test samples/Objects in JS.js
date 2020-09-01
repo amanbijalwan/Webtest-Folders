acc529e4da0b178f3357ea522bffd6a38ae8d947
Aman777 <amanbijalwan@gmail.com>
2020-09-01 23:21:19 +0530

    New test JS test script

---
+++ JS test samples/Objects in JS.js
@@ -0,0 +1,29 @@
+//Object with username and pass systemLanguage
+
+{	
+	username : "Joy";
+	password : "alive";
+}
+
+var database = [
+	{	
+			username : "Joy";
+			password : "alive";
+	}
+];
+
+var newsfeed = [
+
+	{
+		username : "Jon";
+		timeline : "Hello World!";
+	},
+	{
+		username : "Amar"
+		timeline : "Why is this not green..?"'
+	},
+	{
+		username : "Sumit";
+		timeline : "Will work hard next time.";
+	}
+];
