32548ee7f62d1826e57a0a936daa675911ec1228
amanbijalwan <36789793+amanbijalwan@users.noreply.github.com>
2020-09-07 09:59:49 -0700

    Update script.js

--- JS test samples/External JS/script.js
+++ JS test samples/External JS/script.js
@@ -31,12 +31,13 @@ var newsfeed = [
 
 var usernameprompt = prompt("Your username is?");
 var passwordprompt = prompt("Enter password as well");
+var passconfirm = prompt("Confirm your password please.");
 
 //function for sign in
 
 function signIn(user, pass)
 {
-    if(user === database[0].username && pass === database[0].password)
+    if(user === database[0].username && pass === database[0].password && passwordprompt === passconfirm)
         {
             console.log(newsfeed);
         }
